c4ac2c7b493489ab27c1d52d5bbd79b25f28d6f6
Jorunn Leithe <jorul@stud.ntnu.no>
2020-10-23 21:28:25 +0200

    fix some tests

--- frontend/src/store/actions/actions.ts
+++ frontend/src/store/actions/actions.ts
@@ -12,7 +12,9 @@ import { City, StudentCity } from "../interfaces";
 import { gridColumnGap } from "styled-system";
 import Filter from "../../hooks/Filter/interfaces";
 
-const API = "http://localhost:8000/api";
+const API = "http://it2810-72.idi.ntnu.no:3000/api";
+console.log("API: ", process.env.NODE_ENV);
+
 const OFFSET = 2;
 
 export function GetCities(): ThunkAction<
