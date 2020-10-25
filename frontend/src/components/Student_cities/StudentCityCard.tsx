feb187cdd6f404dca3773294a9ebd98b17db1285
Jorunn Leithe <jorul@stud.ntnu.no>
2020-10-25 03:57:58 +0100

    added details button

--- frontend/src/components/Student_cities/StudentCityCard.tsx
+++ frontend/src/components/Student_cities/StudentCityCard.tsx
@@ -1,36 +1,32 @@
 import React from "react";
-import { Box, Badge, PseudoBox, useColorMode, Icon } from "@chakra-ui/core";
+import {
+  Box,
+  Badge,
+  PseudoBox,
+  useColorMode,
+  Icon,
+  Button,
+  Collapse
+} from "@chakra-ui/core";
+import StarRating from "./StarRating";
+import { StudentCity } from "../../store/interfaces";
 
 type Props = {
-  name: string;
-  landlord: string;
-  totalRating: string;
-  city: string;
+  studentCity: StudentCity;
 };
 
-export default function StudentCityCard({
-  name,
-  landlord,
-  totalRating,
-  city
-}: Props) {
-  const property = {
-    title: name,
-    landlord: landlord,
-    rating: totalRating,
-    city: city
-  };
+export default function StudentCityCard({ studentCity }: Props) {
   const { colorMode } = useColorMode();
-  console.log(totalRating);
-  const rating_number: number = +totalRating;
+
+  const [showDetails, setShowDetails] = React.useState(false);
+  const handleToggleDetails = () => setShowDetails(!showDetails);
+
+  const [showModal, setShowModal] = React.useState(false);
+  const handleToggleModal = () => setShowModal(!showDetails);
 
   return (
     <PseudoBox
       transition="all 200ms linear 0s"
-      _hover={{
-        boxShadow: "lg",
-        transform: "translate(0px, -2px)"
-      }}
       margin="10px"
       rounded="lg"
       boxShadow="md"
@@ -40,7 +36,7 @@ export default function StudentCityCard({
         <Box p="6">
           <Box d="flex" alignItems="baseline">
             <Badge rounded="full" px="2" variantColor="teal">
-              {city}
+              {studentCity.by.navn}
             </Badge>
           </Box>
           <Box
@@ -50,23 +46,48 @@ export default function StudentCityCard({
             lineHeight="tight"
             isTruncated
           >
-            {property.title}
+            {studentCity.navn}
           </Box>
-          <Box d="flex" mt="2" alignItems="center">
-            {Array(5)
-              .fill("")
-              .map((_, i) => (
-                <Icon
-                  name="star"
-                  key={i}
-                  color={i < rating_number ? "teal.500" : "gray.300"}
-                />
-              ))}
-          </Box>
-
           <Box as="span" color="gray.500" fontSize="sm">
-            {"utleier:"} {property.landlord}
+            {"utleier:"} {studentCity.utleier}
           </Box>
+          <StarRating
+            name={"Total vurdering: "}
+            rating={studentCity.vurderingTotal}
+          />
+          <Collapse mt={2} isOpen={showDetails}>
+            <StarRating
+              name={"Fellesareal: "}
+              rating={studentCity.vurderingFellesAreal}
+            />
+            <StarRating
+              name={"Lokasjon: "}
+              rating={studentCity.vurderingLokasjon}
+            />
+            <StarRating name={"Pris: "} rating={studentCity.vurderingPris} />
+            <StarRating
+              name={"Tilstand: "}
+              rating={studentCity.vurderingTilstand}
+            />
+          </Collapse>
+          <Button
+            variantColor="teal"
+            size="xs"
+            onClick={handleToggleModal}
+            mt="2"
+          >
+            {"Legg inn vurdering"}
+          </Button>
+          <Button
+            variantColor="teal"
+            size="xs"
+            variant="outline"
+            onClick={handleToggleDetails}
+            mt="2"
+            ml="4"
+          >
+            {showDetails ? "Skjul detaljer" : "Vis detaljer"}
+          </Button>
         </Box>
       </a>
     </PseudoBox>
--- frontend/src/components/Student_cities/index.tsx
+++ frontend/src/components/Student_cities/index.tsx
@@ -28,14 +28,7 @@ export default function Student_cities() {
       bg={colorMode === "light" ? "gray.100" : "gray.700"}
     >
       {studentCities.map(studentCity => {
-        return (
-          <StudentCityCard
-            name={studentCity.navn}
-            landlord={studentCity.utleier}
-            totalRating={studentCity.vurderingTotal}
-            city={studentCity.by.navn}
-          ></StudentCityCard>
-        );
+        return <StudentCityCard studentCity={studentCity}></StudentCityCard>;
       })}
     </Box>
   );
