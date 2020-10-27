ad3a26d1c42b43ee0186efc195d1a5a4188559ac
Jorunn Leithe <jorul@stud.ntnu.no>
2020-10-27 18:41:55 +0100

    updated colors, removed icon in upper left corner

--- frontend/src/components/Container/index.tsx
+++ frontend/src/components/Container/index.tsx
@@ -4,15 +4,17 @@ import Student_cities from "../Student_cities";
 import { Box, color, useColorMode } from "@chakra-ui/core";
 
 const Container = () => {
-  const { colorMode, toggleColorMode } = useColorMode();
-  const bgColor = { light: "gray.300", dark: "gray.600" };
+  const { colorMode } = useColorMode();
+  const bgColor = { light: "white", dark: "gray.600" };
 
-  return <Box width="100vw" mx="auto" px={6} bg={bgColor[colorMode]}>
-    <Box width="full" mx="auto" maxWidth="1280px">
-      <Search_parameters />
-      <Student_cities />
+  return (
+    <Box width="100vw" mx="auto" px={6} bg={bgColor[colorMode]}>
+      <Box width="full" mx="auto" maxWidth="1280px">
+        <Search_parameters />
+        <Student_cities />
+      </Box>
     </Box>
-    </Box>;
+  );
 };
 
 export default Container;
